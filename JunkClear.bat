199cdd29165e59d5f8e0b49be0b2a26b2cabc3ad
tsymiar <909006258@qq.com>
2019-03-24 22:57:03 +0800

    update build files to solv ci error

--- JunkClear.bat
+++ JunkClear.bat
@@ -1,4 +1,5 @@
 @echo off
+setlocal enabledelayedexpansion
 
 echo ----------------------------------------------------
 echo Clear File
@@ -6,10 +7,32 @@ echo Delete all files with ending:
 echo *.aps *.idb *.ncp *.obj *.pch *.tmp *.sbr *.suo *.sdf *.suo
 echo Visual c++/.Net junk 
 echo ----------------------------------------------------
+echo waitting 6s...
 ping 127.0.0.1 -n 6 >nul
+
+echo deleting complied files...
+rmdir /s/q %~dp0ThirdFunc\boost_1_60_0
+rmdir /s/q %~dp0ThirdFunc\openssl-1.1.1a
 
 del /F /Q /S *.aps *.idb *.ncp *.obj *.pch *.sbr *.user *.tmp *.pdb *.bsc *.ilk *.res *.ncb *.opt *.suo *.manifest *.dep *.sdf *.suo
 
-ping 127.0.0.1 -n 6 >nul
+set b=.*/-\ /-\ *
+set ËÙ¶È=1
+set ÍË¸ñ=
+:b
+for /l %%i in (100,-1,1) do (
+    call :a %%i
+)
+:a
+set/a a=%1%%10
+set/a c=%a%%%4
+if %a% EQU 0 set/p=.<nul
+if %c% EQU 3 (set/p=^|<nul) else (set/p=!b:~%a%,1!<nul)
+ping -n %ËÙ¶È% 127.1>nul
+set/p=%ÍË¸ñ%<nul
+if errorlevel 1 (
+	goto :eof
+)
+
 
 
--- build.bat
+++ build.bat
@@ -9,9 +9,13 @@ set BATDIR=%~dp0
 echo ----------------------------------------------------
 echo Extract pthread files
 echo ----------------------------------------------------
-cd lib-src
-7za x pthreads-w32-2-9-1-release.zip -o%BATDIR%ThirdFunc\pthread -y
-cd %BATDIR%
+if not exist "%BATDIR%ThirdFunc\pthread\Pre-built.2\lib" (
+    cd lib-src
+    7za x pthreads-w32-2-9-1-release.zip -o%BATDIR%ThirdFunc\pthread -y
+    cd %BATDIR%
+) else (
+    echo pthread lib exist, skip extract pthread!
+)
 
 echo ----------------------------------------------------
 echo Setup libmysql libs
@@ -26,65 +30,77 @@ if not exist "%BATDIR%ThirdFunc\libmysql\libmysql.lib" (
 :    %BATDIR%lib-src\7za x libmysql.zip -o.
 :    expand libmysql.zip -F:libmysql.* .
 :    explorer libmysql.zip
-    cd %BATDIR%lib-src
+    cd lib-src
     7za x mysql-connector-c-6.1.11-win32.zip -o%BATDIR%ThirdFunc\libmysql
+    color e4
     cd %BATDIR%ThirdFunc\libmysql
     copy mysql-connector-c-6.1.11-win32\lib\* .
     rd /s /q %BATDIR%ThirdFunc\libmysql\mysql-connector-c-6.1.11-win32
-
+:    echo Waitting for unzip libmysql.lib(.dll) to %BATDIR%ThirdFunc\libmysql
+    color 07
+    cd %BATDIR%
+) else (
+    echo libmysql.lib exist, skip setup mysql!
 )
-color e4
-echo Waitting for unzip libmysql.lib(.dll) to %BATDIR%ThirdFunc\libmysql
-color 07
-cd %BATDIR%
 
 echo ----------------------------------------------------
 echo Compile boost libraries
 echo ----------------------------------------------------
-cd lib-src
-if not exist "%BOOST%" (
+set BOOST=%BATDIR%ThirdFunc\boost_1_60_0
+if not exist "%BOOST%\stage\lib" (
+    cd lib-src
 :    set /p BOOST="set boost location:\n"
 :    if "%BOOST%"=="" set BOOST=%BATDIR%ThirdFunc\boost_1_60_0
     7za x boost_1_60_0.7z -o%BATDIR%ThirdFunc
+    cd %BOOST%
+    if not exist bjam (
+        call bootstrap.bat
+    )
+    echo compiling boost stage...
+    bjam stage architecture=x86 address-model=32 --with-atomic --with-math --with-date_time --with-thread --with-filesystem --with-system --with-regex link=static runtime-link=shared threading=multi variant=debug
+    cd %BATDIR%
+) else (
+    echo boost libraries exist, skip compile boost!
 )
-cd %BATDIR%ThirdFunc\boost_1_60_0
-if not exist bjam (
-    call bootstrap.bat
-)
-bjam stage architecture=x86 address-model=32 --with-atomic --with-math --with-date_time --with-thread --with-filesystem --with-system --with-regex link=static runtime-link=shared threading=multi variant=debug
-cd %BATDIR%
 
 echo ----------------------------------------------------
 echo Compile openssl library
 echo ----------------------------------------------------
-cd lib-src
-msiexec /package ActivePerl-5.20.2.2001-MSWin32-x64-298913.msi /qb
-echo finish install ActivePerl
-set SSLDIR=%BATDIR%ThirdFunc\openssl-1.1.1a
-7za x openssl-1.1.1a.tar.gz -y
-7za x openssl-1.1.1a.tar -o%SSLDIR%\.. -y
-copy /y opensslconf.h %SSLDIR%\include\openssl
-cd %SSLDIR%
-call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
-perl %SSLDIR%\Configure VC-WIN32 --prefix=%SSLDIR%
-if errorlevel 1 (
-    echo fail to set perl Configure.
+if not exist "%BATDIR%ThirdFunc\openssl-1.1.1a\lib\libcrypto.lib" (
+    cd lib-src
+    msiexec /package ActivePerl-5.20.2.2001-MSWin32-x64-298913.msi /qb
+    echo finish install ActivePerl
+    set SSLDIR=%BATDIR%ThirdFunc\openssl-1.1.1a
+    if not exist "%SSLDIR%" (
+        md "%SSLDIR%"
+    )
+    7za x openssl-1.1.1a.tar.gz -y
+    7za x openssl-1.1.1a.tar -o%SSLDIR%\.. -y
+    copy /y opensslconf.h %SSLDIR%\include\openssl
+    cd %SSLDIR%
+    call "C:\Program Files (x86)\Microsoft Visual Studio 14.0\VC\vcvarsall.bat"
+    perl %SSLDIR%\Configure VC-WIN32 --prefix=%SSLDIR%
+    if errorlevel 1 (
+        echo fail to load perl Configure.
+        cd %BATDIR%
+        goto :eof
+    )
+    nmake
+    nmake install
+    if errorlevel 1 (
+        echo fail to make/install ntlib.
+    )
+    cd %BATDIR%lib-src
+    7za x openssl-1.1.1a.tar -o%SSLDIR%\.. -y 
+    del openssl-1.1.1a.tar
     cd %BATDIR%
-    goto :eof
+) else (
+    echo libcrypto exist, skip compile openssl!
 )
-nmake && nmake install
-if errorlevel 1 (
-    echo fail to make/install ntlib.
-)
-cd %BATDIR%lib-src
-7za x openssl-1.1.1a.tar -o%SSLDIR%\.. -y 
-del openssl-1.1.1a.tar
-cd %BATDIR%
 
 echo ----------------------------------------------------
 echo Build AllServer solution
 echo ----------------------------------------------------
-set BOOST="%BATDIR%ThirdFunc\boost_1_60_0"
 set PTHREAD="%BATDIR%ThirdFunc\pthread\Pre-built.2"
 "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\MSBuild\15.0\Bin\MSBuild.exe" ./AllServer.sln /t:build /p:configuration="Debug" /m
 if errorlevel 1 (
